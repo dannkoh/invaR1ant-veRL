49a3f1bdf66db3d6ccd0b6783cb352ae7ea444f5
Daniel Koh <daniel.koh@student.manchester.ac.uk>
2025-02-25 21:37:31 +0000

    upload instruct

--- scripts/qwen2.5-3b-instruct-ppo.sh
+++ scripts/qwen2.5-3b-instruct-ppo.sh
@@ -3,17 +3,17 @@ set -x
 export VLLM_ATTENTION_BACKEND=XFORMERS
 
 python3 -u -m verl.trainer.main_ppo \
-    data.train_files=$HOME/ConStruct-veRL/data/train.parquet \
-    data.val_files=$HOME/ConStruct-veRL/data/test.parquet \
-    data.train_batch_size=1024 \
-    data.val_batch_size=1024 \
+    data.train_files=$HOME/ConStruct-veRL/data/train-instruct.parquet \
+    data.val_files=$HOME/ConStruct-veRL/data/test-instruct.parquet \
+    data.train_batch_size=256 \
+    data.val_batch_size=128 \
     data.max_prompt_length=1800 \
-    data.max_response_length=4096 \
+    data.max_response_length=2048 \
     data.instruct=True \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-3B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=128 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=80 \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
@@ -21,7 +21,7 @@ python3 -u -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=160 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=160 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     critic.optim.lr=1e-5 \
@@ -38,6 +38,6 @@ python3 -u -m verl.trainer.main_ppo \
     trainer.experiment_name='qwen2.5-3b-instruct-ppo' \
     trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
-    trainer.save_freq=100 \
-    trainer.test_freq=100 \
-    trainer.total_epochs=15 $@
+    trainer.save_freq=10 \
+    trainer.test_freq=5 \
+    trainer.total_epochs=2 $@
