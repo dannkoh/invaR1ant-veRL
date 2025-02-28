6608ebbde243927bda78d773515c76721fb117b9
daniel <daniel.koh@student.manchester.ac.uk>
2025-02-28 18:57:59 +0000

    remove need to check for assistant for instruct

--- verl/utils/reward_score/custom.py
+++ verl/utils/reward_score/custom.py
@@ -152,7 +152,6 @@ def extract_solution(response: str, is_instruct: bool) -> str:
         )
     else:
         pattern = (
-            r".*<|im_start|>assistant\s*.*?"      # Allow any text (including the prompt) before the tokens.
             r"<think>\s*(.*?)\s*</think>\s*"         # Capture chain-of-thought.
             r"<answer>\s*(.*?)\s*</answer>"       # Capture answer block, with no extra text after.
         )
